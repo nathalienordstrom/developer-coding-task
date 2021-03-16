63cc65d8409ac6b26ba5ca617cfcf04a4629e387
nathalie <nathalie-nordstrom@hotmail.com>
2021-03-16 18:51:34 +0100

    first

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,58 @@
-(function ($) { 
-	
+function animatedForm() {
+    const arrows = document.querySelectorAll(".fa-arrow-down");
+
+    arrows.forEach(arrow => {
+        arrow.addEventListener('click', () => {
+            const input = arrow.previousElementSibling;
+            const parent = arrow.parentElement;
+            const nextForm = parent.nextElementSibling;
+
+            // Check for validation
+
+            if(input.type === "email" && validationEmail(input)) {
+                nextSlide(parent, nextForm)
+            }else if (input.type === "text" && validateUser(input)) {
+                nextSlide(parent, nextForm);
+            }else {
+                parent.style.animation = "shake 0.5s ease";
+            }
+        });
+    });
+}
+
+function validationEmail(email) {
+    const validation = /^[^\s@]+@[^\s@]+$/;
+    if (validation.test(email.value)) {
+        error("rgb(114, 167, 136)");
+        return true;
+    } else {
+        error("rgb(189, 87, 87");
+    }
+}
+
+function validateUser(user) {
+    if (user.value.length < 2) {
+        error("rgb(189, 87, 87");
+    } else {
+        error("rgb(114, 167, 136)");
+        return true;
+    }
+}
+
+function nextSlide(parent, nextForm) {
+    parent.classList.add("inactive");
+    parent.classList.remove("active");
+    nextForm.classList.add("active")
+}
+
+function error(color) {
+    document.body.style.backgroundColor = color;
+}
+
+animatedForm();
+
+
+(function ($) {
+
+
 })(jQuery);
